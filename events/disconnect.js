7addff0894cd61bc0fb67d8b93aeac38f33bc1d3
wesselgame <wesseltip2004@gmail.com>
2017-05-25 12:34:00 +0200

    Disconnect event.

--- events/disconnect.js
+++ events/disconnect.js
@@ -3,3 +3,4 @@ const moment = require('moment');
 module.exports = client => {
    console.log(chalk.green(`[${moment().format(`YYYY-MM-DD HH:mm:ss`)}] - {${__filename}}:`), chalk.yellow(`Client disconnected.`))
  };
+//When client disconnects
