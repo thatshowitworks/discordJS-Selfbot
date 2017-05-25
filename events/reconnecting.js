8652ad20110d4d2f9fe55639fab7d2545d9c222c
wesselgame <wesseltip2004@gmail.com>
2017-05-25 12:35:56 +0200

    Reconnect event.

--- events/reconnecting.js
+++ events/reconnecting.js
@@ -3,3 +3,4 @@ const moment = require('moment');
 module.exports = client => {
    console.log(chalk.green(`[${moment().format(`YYYY-MM-DD HH:mm:ss`)}] - {${__filename}}:`), chalk.yellow(`Client reconnecting.`))
  };
+//When client reconnects
